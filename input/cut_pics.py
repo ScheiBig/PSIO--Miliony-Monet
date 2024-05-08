a931087032d51ca4dbcc342530f289439560dabd
Marcin Jeznach <Scheissig@gmail.com>
2024-05-08 22:06:05 +0200

    cut pictures to only contain belt with notes

---
+++ input/cut_pics.py
@@ -0,0 +1,23 @@
+import os
+import skimage as ski
+import numpy as np
+
+import utils.ansi as ansi
+
+list_dir = [i for i in os.listdir("./input/raw_pics") if i.endswith(".jpg")]
+print()
+
+for i, im in enumerate(list_dir):
+	prog = int(i / (len(list_dir) - 1) * 40)
+
+	ansi.progress(
+		prog, 40, 
+		returns= True, 
+		label= f"cutting: {im:<20}", 
+		final= f"done cutting {str():<16}"
+	)
+
+	img: np.ndarray = ski.io.imread(f"./input/raw_pics/{im}")
+	new_img: np.ndarray = img[80:-50, 60:-90].copy()
+
+	ski.io.imsave(f"./input/cut_pics/{im}", new_img)
